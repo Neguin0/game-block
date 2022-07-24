285f8e38ec72c60a9832bdc5735763ca853f5f2a
Neguin0 <cs93558889@gmail.com>
2022-07-24 01:46:25 -0300

    edit port

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const { server } = require('./src/app');
 
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-	console.log('listening on *:'+port);
+server.listen(80, () => {
+	console.log('listening on *:80');
 });
